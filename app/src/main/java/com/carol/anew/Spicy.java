5160184a5bfa72092c136401a4057a69ea3af475
CarolMusyoka <CarolMusyoka@.github.com>
2018-11-19 09:56:35 +0300

    Initial commit

---
+++ app/src/main/java/com/carol/anew/Spicy.java
@@ -0,0 +1,13 @@
+package com.carol.anew;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Spicy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spicy);
+    }
+}
